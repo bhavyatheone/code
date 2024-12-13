b75f3384f8aee62190b11c49ead266d57ce04198
bhavyatheone <bbhavya2304@gmail.com>
2024-12-13 09:39:34 +0530

    squarestar.c

---
+++ squarestar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int i,j,c;
+ printf("enter the char :");scanf("%d",&c);
+      for (int i =1; i <=c; ++i)
+{
+                  //printf("%c \n",flag);
+                  for (int j = 1; j<=c ;j++)
+                  {
+                    printf("*");
+                  }
+  
+  //  for (int j = 0; j <=4; j++) {        printf("%c \n ",c);}printf("",c);
+
+printf("\n");
+} 
+    return 0;
+}
